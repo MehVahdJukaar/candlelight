cb6c45740e0e4dfaa27171ec39f866e46a6ba2f5
Niklas Widmann <nik@somethingcatchy.net>
2026-05-06 12:32:33 +0200

    support unit testing

--- src/main/java/net/mehvahdjukaar/candlelight/core/CandleLightPlugin.java
+++ src/main/java/net/mehvahdjukaar/candlelight/core/CandleLightPlugin.java
@@ -4,14 +4,16 @@ import net.mehvahdjukaar.candlelight.core.jars_processors.ClientOnlyTransformPlu
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.file.Directory;
-import org.gradle.api.plugins.JavaPluginExtension;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.compile.JavaCompile;
-import org.gradle.jvm.tasks.Jar;
+
+import javax.annotation.Nullable;
 
 public class CandleLightPlugin implements Plugin<Project> {
 
     private static final String PREFIX = "[CANDLELIGHT] ";
+
+    private static final String TASK_NAME = "candleLightTransform";
 
     public static void log(Project project, String s) {
         project.getLogger().lifecycle(PREFIX + s);
@@ -29,90 +31,116 @@ public class CandleLightPlugin implements Plugin<Project> {
 
         ClientOnlyTransformPlugin.apply(project, clExtension);
 
-
         project.getPlugins().withId("java", plugin -> {
-
-            JavaCompile compileTask =
-                    (JavaCompile) project.getTasks().getByName("compileJava");
-
-            // =====================================
-            // 1. MOVE compile output FIRST (safe)
-            // =====================================
-            Provider<Directory> rawDir =
-                    project.getLayout().getBuildDirectory().dir("raw/classes");
-
-            Provider<Directory> finalDir =
-                    project.getLayout().getBuildDirectory().dir("classes/java/main");
-
-            compileTask.getDestinationDirectory().set(rawDir);
-
-            // =====================================
-            // 2. TRANSFORM TASK (no cycle)
-            // =====================================
-            var transformTask = project.getTasks().register(
-                    "candleLightTransform",
-                    TransformClassesTask.class,
-                    t -> {
-
-                        // read compiled raw output
-                        t.getSourceDir().set(rawDir);
-
-                        // write FINAL runtime output
-                        t.getOutputDir().set(finalDir);
-
-                        t.getExtensionProperty().set(clExtension);
-
-                        t.dependsOn(compileTask);
-                    }
+            transformCompileTask(
+                    project,
+                    (JavaCompile) project.getTasks().getByName("compileJava"),
+                    null
             );
 
-            // =====================================
-            // 3. ensure ordering
-            // =====================================
-            project.getTasks().named("classes", task -> {
-                task.dependsOn(transformTask);
-            });
+            transformCompileTask(
+                    project,
+                    (JavaCompile) project.getTasks().getByName("compileTestJava"),
+                    "test"
+            );
 
             project.getTasks().configureEach(task -> {
                 String taskName = task.getName();
                 if (taskName.contains("remapSourcesJar") || taskName.contains("remapJar")) {
-                    task.dependsOn(transformTask);
+                    task.dependsOn(TASK_NAME);
                     project.getRootProject().getChildProjects().values().forEach(p -> {
                         if (p.getName().equals("common")) {
-                            task.dependsOn(p.getTasks().named("candleLightTransform"));
+                            task.dependsOn(TASK_NAME);
                         }
                     });
                 }
-                if (taskName.equals("copyAccessTransformersPublications")) {
-                    task.dependsOn(project.getTasks().named("transformAccessWidener"));
-                }
+
                 if (taskName.equals("curseforge")) {
-                    task.dependsOn(project.getTasks().named("jar"));
+                    task.dependsOn("jar");
                 }
             });
-
-            if (project.getName().equals("fabric")) {
-
-                project.getTasks().named("compileJava", t -> {
-                    t.dependsOn(
-                            project.project(":common")
-                                    .getTasks()
-                                    .named("candleLightTransform")
-                    );
-                });
-            }
-
-            if (project.getName().equals("neoforge")) {
-
-                project.getTasks().named("compileJava", t -> {
-                    t.dependsOn(
-                            project.project(":common")
-                                    .getTasks()
-                                    .named("candleLightTransform")
-                    );
-                });
-            }
-
         });
     }
+
+    private void transformCompileTask(Project project, JavaCompile compileTask, @Nullable String key) {
+        var clExtension = project.getExtensions().getByType(CandleLightExtension.class);
+
+        // =====================================
+        // 1. MOVE compile output FIRST (safe)
+        // =====================================
+        Provider<Directory> rawDir =
+                project.getLayout().getBuildDirectory().dir("raw/" + compileTask.getName() + "/classes");
+
+        var outputFolder = key == null ? "main" : key;
+        Provider<Directory> finalDir =
+                project.getLayout().getBuildDirectory().dir("classes/java/" + outputFolder);
+
+        compileTask.getDestinationDirectory().set(rawDir);
+
+        // =====================================
+        // 2. TRANSFORM TASK (no cycle)
+        // =====================================
+        var suffix = key == null ? "" : capitalize(key);
+        var transformTaskName = TASK_NAME + suffix;
+        var transformTask = project.getTasks().register(
+                transformTaskName,
+                TransformClassesTask.class,
+                t -> {
+
+                    // read compiled raw output
+                    t.getSourceDir().set(rawDir);
+
+                    // write FINAL runtime output
+                    t.getOutputDir().set(finalDir);
+
+                    t.getExtensionProperty().set(clExtension);
+
+                    t.dependsOn(compileTask);
+
+                    t.onlyIf($ -> compileTask.getEnabled());
+                }
+        );
+
+        // =====================================
+        // 3. ensure ordering
+        // =====================================
+        var classesTask = key == null ? "classes" : (key + "Classes");
+        project.getTasks().named(classesTask, task -> {
+            task.dependsOn(transformTask);
+        });
+
+        /*
+         * This only works for one specific project setup:
+         * - root
+         *   - :common
+         *   - :fabric (depends on :common)
+         *   - :neoforge (depends on :common)
+         */
+        if (project.getName().equals("fabric")) {
+
+            project.getTasks().named(compileTask.getName(), t -> {
+                t.dependsOn(
+                        project.project(":common")
+                                .getTasks()
+                                .named(transformTaskName)
+                );
+            });
+        }
+
+        if (project.getName().equals("neoforge")) {
+
+            project.getTasks().named(compileTask.getName(), t -> {
+                t.dependsOn(
+                        project.project(":common")
+                                .getTasks()
+                                .named(transformTaskName)
+                );
+            });
+        }
+    }
+
+    private static String capitalize(String input) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
+    }
+
 }
